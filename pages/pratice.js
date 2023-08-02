afe5fa3a094223f340d2500a713d65927f79f91c
zerospeedzero <zerospeedzero@gmail.com>
2023-08-01 21:49:14 -0600

    Use Next JS to host the game

--- pages/pratice.js
+++ pages/pratice.js
@@ -2,7 +2,7 @@ import React from 'react'
 const testing = () => {
   return (
     <div className="w-full flex flex-col justify-center items-center pt-[100px]">
-      <iframe src={"http://dev.saitnewmedia.ca/~gcheng/practice/index.html"}
+      <iframe src={"/practice/index.html"}
         className="w-[full] max-w-[1000px] aspect-square border-2 shadow-lg rounded-lg"
         width="100%"
         height="auto" 
